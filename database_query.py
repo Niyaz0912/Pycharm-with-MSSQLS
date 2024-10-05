6df28f09a4a2f8237de31165732556025ef4244f
Niyaz Khasanov <khasanchik85@gmail.com>
2024-10-05 20:30:41 +0500

    Создан пайтон файл database_query.py с запросами к БД MSSQLS

---
+++ database_query.py
@@ -0,0 +1,66 @@
+import os
+from dotenv import load_dotenv
+import pyodbc
+
+load_dotenv()
+SERVER = os.getenv('MS_SQL_SERVER')
+DATABASE = os.getenv('MS_SQL_DATABASE')
+USER = os.getenv('MS_SQL_USER')
+PASSWORD = os.getenv('MS_SQL_KEY')
+
+# Создаем строку подключения
+connection_string = f'DRIVER={{SQL Server}};SERVER={SERVER};DATABASE={DATABASE};USER={USER};PASSWORD={PASSWORD}'
+
+# Устанавливаем соединение
+try:
+    cnxn = pyodbc.connect(connection_string)
+    cursor = cnxn.cursor()
+    print("Подключение успешно!")
+except Exception as e:
+    print(f"Ошибка подключения: {e}")
+
+
+# Создание Базы данных
+try:
+    cursor.execute("CREATE DATABASE TestDB")
+    print("База данных создана!")
+except Exception as e:
+    print(f"Ошибка при создании базы данных: {e}")
+
+
+# Создание таблицы
+try:
+    cursor.execute("""
+    CREATE TABLE Users (
+        ID INT PRIMARY KEY IDENTITY(1,1),
+        Name NVARCHAR(50),
+        Age INT
+    )
+    """)
+    print("Таблица создана!")
+except Exception as e:
+    print(f"Ошибка при создании таблицы: {e}")
+
+
+# Заполнение таблицы данными
+try:
+    cursor.execute("INSERT INTO Users (Name, Age) VALUES ('Иван', 30)")
+    cursor.execute("INSERT INTO Users (Name, Age) VALUES ('Анна', 25)")
+    cnxn.commit()  # Подтверждение изменений
+    print("Данные добавлены в таблицу!")
+except Exception as e:
+    print(f"Ошибка при добавлении данных: {e}")
+
+
+# Вывод данных из базы данных
+try:
+    cursor.execute("SELECT * FROM Users")
+    rows = cursor.fetchall()
+
+    users_list = []
+    for row in rows:
+        users_list.append({'ID': row.ID, 'Name': row.Name, 'Age': row.Age})
+
+    print("Данные из таблицы:", users_list)
+except Exception as e:
+    print(f"Ошибка при выводе данных: {e}")
